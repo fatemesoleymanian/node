1912ada448ccdb1d0f63e266460f0306e8845270
fatemesoleymanian <soleymanian.usc@gmail.com>
2023-03-25 02:43:52 -0700

    l1-express

---
+++ 02-express-tutorial/lesson-1/myApp.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let app = express();
+
+console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
